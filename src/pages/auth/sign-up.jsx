087bb3b8b9b310f7644545f23c931150602e7f46
rhw <qorgh2akfl@naver.com>
2023-03-30 00:00:50 +0900

    230329_rhw_signup

--- src/pages/auth/sign-up.jsx
+++ src/pages/auth/sign-up.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { React, useState, useEffect, useRef } from "react";
 import {
   Card,
   CardHeader,
@@ -11,6 +12,67 @@ import {
 } from "@material-tailwind/react";
 
 export function SignUp() {
+
+  function onChangeSignUp(e) {
+    console.log("e_onchange",e)
+    
+    const value = e.target.value
+    const name = e.target.name
+    console.log("value",value)
+    console.log("name",name)    
+    setSignUpInfo({...signUpInfo,[name]:value}); // 이전 정보들은 넣어놓고, id면 id 만 pw면 pw만 바꿔서 갱신하기
+    // 이렇게 전역 id/pw값을 계속 갱신하고, 제줄버튼을 누르게 되면 전역 id/pw를 post하기
+  }
+
+  function handleSignUpSubmit(e) {
+    console.log(e)
+    e.preventDefault();
+    console.log("id/pw/sk",id,password,specialKey)
+
+    // 여기서 post하기
+
+    // let posturl = "https://farm01.bitlworks.co.kr/api/v1/";
+    // let posturl_set = posturl + "auth" + "/" + "login";
+    // console.log("posturl:", posturl_set);
+    
+
+    // axios
+    //   .post(posturl_set, data_t, config)
+    //   .then((response) => {
+    //     console.log(response.status);
+    //     console.log(response.data);
+    //     setCookie("cookie", response.data);
+
+    //     window.location.replace("/Main");
+    //   })
+      
+    //   .catch((error) => {
+    //     alert("아이디와 비밀번호를 다시 확인하세요");
+    //     console.log("re:", error.message);
+    //     console.log("re:", error.body);
+    //     console.log("re:", error.config);
+    //     console.log("re:", error.requests);
+    //     console.log("re:", error.response.data);
+    //   });
+
+
+
+    // 임시 로그인
+    if(id == "root" & password == "1111"& specialKey == "1234"){
+      alert("회원가입 완료")
+    }else{
+      alert("아이디와 비밀번호를 확인하세요")
+    }
+
+
+  }
+  const [signUpInfo, setSignUpInfo] = useState({
+    id: "",
+    password: "",
+    specialKey:""
+  });
+   
+  const {id, password,specialKey} = signUpInfo; // 전역 id/pw 선언
   return (
     <>
       <img
@@ -19,43 +81,48 @@ export function SignUp() {
       />
       <div className="absolute inset-0 z-0 h-full w-full bg-black/50" />
       <div className="container mx-auto p-4">
-        <Card className="absolute top-2/4 left-2/4 w-full max-w-[24rem] -translate-y-2/4 -translate-x-2/4">
-          <CardHeader
-            variant="gradient"
-            color="blue"
-            className="mb-4 grid h-28 place-items-center"
-          >
-            <Typography variant="h3" color="white">
-              Sign Up
-            </Typography>
-          </CardHeader>
-          <CardBody className="flex flex-col gap-4">
-            <Input label="Name" size="lg" />
-            <Input type="email" label="Email" size="lg" />
-            <Input type="password" label="Password" size="lg" />
-            <div className="-ml-2.5">
-              <Checkbox label="I agree the Terms and Conditions" />
-            </div>
-          </CardBody>
-          <CardFooter className="pt-0">
-            <Button variant="gradient" fullWidth>
-              Sign Up
-            </Button>
-            <Typography variant="small" className="mt-6 flex justify-center">
-              Already have an account?
-              <Link to="/auth/sign-in">
-                <Typography
-                  as="span"
-                  variant="small"
-                  color="blue"
-                  className="ml-1 font-bold"
-                >
-                  Sign in
-                </Typography>
-              </Link>
-            </Typography>
-          </CardFooter>
-        </Card>
+        <form
+          onSubmit={handleSignUpSubmit}
+        >
+          <Card className="absolute top-2/4 left-2/4 w-full max-w-[24rem] -translate-y-2/4 -translate-x-2/4">
+            <CardHeader
+              variant="gradient"
+              color="blue"
+              className="mb-4 grid h-28 place-items-center"
+            >
+              <Typography variant="h3" color="white">
+                Sign Up
+              </Typography>
+            </CardHeader>
+            <CardBody className="flex flex-col gap-4">
+              
+              <Input type="text" label="Id" name = "id" size="lg" onChange = {onChangeSignUp}/>
+              <Input type="password" label="Password" name = "password" size="lg" onChange = {onChangeSignUp}/>
+              <Input label="SpecialKey" size="lg" name = "specialKey" onChange = {onChangeSignUp} />
+              <div className="-ml-2.5">
+                <Checkbox label="I agree the Terms and Conditions" />
+              </div>
+            </CardBody>
+            <CardFooter className="pt-0">
+              <Button variant="gradient" type = "submit" fullWidth>
+                Sign Up
+              </Button>
+              <Typography variant="small" className="mt-6 flex justify-center">
+                Already have an account?
+                <Link to="/auth/sign-in">
+                  <Typography
+                    as="span"
+                    variant="small"
+                    color="blue"
+                    className="ml-1 font-bold"
+                  >
+                    Sign in
+                  </Typography>
+                </Link>
+              </Typography>
+            </CardFooter>
+          </Card>
+        </form>
       </div>
     </>
   );
